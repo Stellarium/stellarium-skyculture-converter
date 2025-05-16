1936c77c90e4e072cab3bc2e9bc22f7815645007
Mher Mnatsakanyan <mher.mnatsakanyan03@gmail.com>
2025-05-16 17:12:49 +0200

    Changes according to guidelines, clarifications

--- main.cpp
+++ main.cpp
@@ -3,49 +3,67 @@
 #include <vector>
 #include <string>
 #include <QCoreApplication>
-#include "Converter.hpp"
+#include "SkyCultureConverter.hpp"
 #include "Utils.hpp"
 
-int usage(const char* argv0, const int ret)
+int usage(const char *argv0, const int ret)
 {
-	auto& out = ret ? std::cerr : std::cout;
-	out << "Usage: " << argv0 << " [options...] skyCultureDir outputDir [skyCulturePoDir]\n"
-	    << "Options:\n"
-	    << "  --footnotes-to-references  Try to convert footnotes to references\n"
-	    << "  --untrans-names-are-native Record untranslatable star/DSO names as native names\n"
-	    << "  --native-locale LOCALE     Use *_names.LOCALE.fab as a source for \"native\" constellation names (the\n"
-	       "                             middle column in *_names.eng.fab will be moved to the \"pronounce\" entry.\n"
-	    << "  --translated-md            Generate localized Markdown files (for checking translations)\n";
-	return ret;
+    auto &out = ret ? std::cerr : std::cout;
+    out << "Usage: " << argv0 << " [options...] skyCultureDir outputDir [skyCulturePoDir]\n"
+        << "Options:\n"
+        << "  --footnotes-to-references  Try to convert footnotes to references\n"
+        << "  --untrans-names-are-native Record untranslatable star/DSO names as native names\n"
+        << "  --native-locale LOCALE     Use *_names.LOCALE.fab as a source for \"native\" constellation names (the\n"
+           "                             middle column in *_names.eng.fab will be moved to the \"pronounce\" entry.\n"
+        << "  --translated-md            Generate localized Markdown files (for checking translations)\n";
+    return ret;
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
     QString inDir, outDir, poDir, nativeLocale;
     bool footnotesToRefs = false, genTranslatedMD = false, convertUntranslatableNamesToNative = false;
     // parse arguments
-    std::vector<std::string> args(argv+1, argv+argc);
-    for(const auto& arg : args) {
-        if(!arg.empty() && arg[0] != '-') {
-            if(inDir.isEmpty()) inDir = arg.c_str();
-            else if(outDir.isEmpty()) outDir = arg.c_str();
-            else if(poDir.isEmpty()) poDir = arg.c_str();
-            else return usage(argv[0], 1);
-        } else if(arg == "--translated-md") genTranslatedMD = true;
-        else if(arg == "--footnotes-to-references") footnotesToRefs = true;
-        else if(arg == "--untrans-names-are-native") convertUntranslatableNamesToNative = true;
-        else if(arg == "--native-locale") {
+    std::vector<QString> args(argv + 1, argv + argc);
+    for (const auto &arg : args)
+    {
+        if (!arg.isEmpty() && arg[0] != '-')
+        {
+            if (inDir.isEmpty())
+                inDir = arg;
+            else if (outDir.isEmpty())
+                outDir = arg;
+            else if (poDir.isEmpty())
+                poDir = arg;
+            else
+                return usage(argv[0], 1);
+        }
+        else if (arg == "--translated-md")
+            genTranslatedMD = true;
+        else if (arg == "--footnotes-to-references")
+            footnotesToRefs = true;
+        else if (arg == "--untrans-names-are-native")
+            convertUntranslatableNamesToNative = true;
+        else if (arg == "--native-locale")
+        {
             static bool nextIsLocale = false;
             nextIsLocale = !nextIsLocale;
-            if(nextIsLocale) continue;
-            nativeLocale = arg.c_str();
-        } else if(arg == "--help" || arg == "-h") {
+            if (nextIsLocale)
+                continue;
+            nativeLocale = arg;
+        }
+        else if (arg == "--help" || arg == "-h")
+        {
             return usage(argv[0], 0);
-        } else return usage(argv[0], 1);
+        }
+        else
+            return usage(argv[0], 1);
     }
     // invoke converter
-    return Converter::convert(inDir, outDir, poDir, nativeLocale,
-                              footnotesToRefs, genTranslatedMD,
-                              convertUntranslatableNamesToNative);
+    int result = SkyCultureConverter::convert(inDir, outDir, poDir, nativeLocale,
+                                              footnotesToRefs, genTranslatedMD,
+                                              convertUntranslatableNamesToNative);
+    std::cout << "SkyCultureConverter::\tConversion return-code: " << result << "\n";
+    return result;
 }
